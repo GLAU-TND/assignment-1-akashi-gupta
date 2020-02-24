0ea139ce3f6a5dd09c4536285a3749c929ca365c
akashi <akashi.gupta_cs18@gla.ac.in>
2020-02-24 15:29:21 +0530

    MyLinkedList class variable and there getter and setter methods to accesss variables

---
+++ src/definition/MyLinkedList.java
@@ -0,0 +1,28 @@
+package definition;
+
+
+import Helper.Node;
+
+public class MyLinkedList<T extends Comparable<T>> {
+
+    private Node<T> head;           //It is the beginning of linked list
+    private Node<T> iter;           //It is used to iterate the list
+
+    public Node<T> getHead() {
+        return head;
+    }        //getter for head
+
+    public void setHead(Node<T> node) {
+        head = node;
+        iter = head;
+    }   //setter for head
+
+    public Node<T> getIter() {
+        return iter;
+    }                //Getter for iter
+
+    public void setIter(Node<T> iter) {
+        this.iter = iter;
+    }  //Setter for iter
+
+}
