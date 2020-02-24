458ea91eb2a3b049e21144f7b5e7c93de3d7b01d
akashi <akashi.gupta_cs18@gla.ac.in>
2020-02-24 15:34:05 +0530

    Menu class to menupulate operations variables and constructor

---
+++ src/definition/Menu.java
@@ -0,0 +1,17 @@
+package definition;
+
+import Helper.Person;
+
+import java.util.Scanner;
+
+public class Menu {
+    private MyLinkedList<Person> linkedList;
+    private Scanner scan;
+
+    public Menu() {
+        scan = new Scanner(System.in);
+        linkedList = new MyLinkedList<>();
+    }
+
+
+}
